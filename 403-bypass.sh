d5aa32667a9e84f2f9ce628d7605b46b40754452
OsmanTunahan <osmantunahan007@gmail.com>
2024-10-28 11:15:20 +0300

    Add colors

--- 403-bypass.sh
+++ 403-bypass.sh
@@ -1,6 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
 SCRIPT_NAME="403-Bypass"
 AUTHOR="Osman Tunahan ARIKAN"
 TARGET_URL="$1"
 TARGET_PATH="$2"
+
+function color_code() {
+    local status=$1
+    if [[ $status -eq 200 ]]; then echo -e "\e[32m$status\e[0m";
+    elif [[ $status -eq 403 ]]; then echo -e "\e[31m$status\e[0m";
+    else echo -e "\e[33m$status\e[0m";
+    fi
+}
